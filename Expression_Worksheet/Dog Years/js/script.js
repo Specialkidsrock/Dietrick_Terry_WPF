c7e7be629a3c8fac80099d58b0b04c13f40359e0
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-07 22:07:41 -0500

    Dog years js created
    JS created and variables defined and solutions tested

--- Expression_Worksheet/Dog Years/js/script.js
+++ Expression_Worksheet/Dog Years/js/script.js
@@ -1,1 +1,11 @@
-//
+//Terry Dietrick 07 December 2014 Dog Years
+
+// Calculate dog years based off of human years inputted
+
+var dogYears; //Variable for dog years
+var humanYears; // Variable for human years
+
+// Calculate dog years based off human years input
+
+
+
