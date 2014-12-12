37ef570d77f0843ba91e34737e4404b08c86e261
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-12 02:05:11 -0500

    Conditionals_Industry code added
    Identified the variables to use, and the idea for this assignment

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -2,3 +2,11 @@
  SDI Section 04WDD
 Conditionals Industry
  */
+
+alert("Is your monitor 4K resolution? Enter the values ");
+var horizontalPixelsvalue = prompt("Please enter your monitors vertical pixel value", 800);
+var verticalPixelvalue = prompt("Please enter your monitors horizontal pixel value",1200);
+
+var uhdResolution = 8294400;
+
+If
