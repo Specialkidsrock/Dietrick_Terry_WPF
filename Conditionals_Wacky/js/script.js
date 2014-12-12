266e134a926afea40da8055a7dcc7b0ad16ab809
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-12 03:14:47 -0500

    Conditionals Wacky code changed
    I corrected the code to display correctly when nothing, or 0 is entered
    into the box. It wasn’t recognizing null

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -8,16 +8,18 @@ Conditionals Wacky
 var milesRan = prompt("Please enter your last run length in miles",3); //prompts the user for the miles they ran
 var timeRan = prompt("How long did it take you to run, please enter in rounded up minutes",24); // prompts the user for the minutes they completed their run in, whole numbers
 var caloriesperMile = 124; // average number of calories burned per mile
+
+if(milesRan == "" || milesRan == 0){
+ milesRan = prompt("Please enter the miles you ran.",2);
+
+}else if(timeRan == "" || timeRan == 0){
+ timeRan = prompt("Please enter a valid number greater then 0 for time ran",1); //used for input validation to ensure there is no other values then numerical
+}
+
 var totalCalorieburn = caloriesperMile * milesRan; //total calorie burn calculation
 var avgminperMile = timeRan / milesRan; //average minute per mile calculation
 
-if(milesRan != 0 && timeRan == ""){
- timeRan = prompt("Please enter a larger numerical value for how long it took you to run, unless you are Usain Bolt!!",20); //used for input validation
- milesRan = prompt("Please enter the miles you ran.",2);
-}
-if(milesRan == 0 || milesRan == null){
- milesRan = prompt("Please enter a valid number greater then 0",1); //used for input validation to ensure there is no other values then numerical
-}else if(milesRan >= 1 && timeRan >= 5) {
+if(milesRan >= 0 && timeRan >= 10) {
  console.log("You burned " + totalCalorieburn + " calories on this run"); //displays the total calories burnt
  console.log("You ran an average of " + avgminperMile + " minutes per mile!"); // displays the average minutes per mile ran
 
