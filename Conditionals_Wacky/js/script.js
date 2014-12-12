2e668ce1070081323afeff074019152cb8b0abf9
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-12 02:33:13 -0500

    Conditionals_Wacky description updated
    Description of what the calculator will try to accomplish updated

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,5 +1,7 @@
 /*Terry Dietrick 11 Dec 2014
- SDI Section 04WDD
+SDI Section 04WDD
 Conditionals Wacky
  */
+
+// This will calculate how many calories you can burn running and how feet per minute you are running!
 
