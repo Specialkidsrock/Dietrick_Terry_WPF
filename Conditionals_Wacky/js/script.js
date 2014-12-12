88c2554f6492edda0ca1013fc751a7c25e79ccb3
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-12 03:04:02 -0500

    Condtionals_Wacky Calculations
    Corrected calculation and input validation to work correctly

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -12,14 +12,14 @@ var totalCalorieburn = caloriesperMile * milesRan; //total calorie burn calculat
 var avgminperMile = timeRan / milesRan; //average minute per mile calculation
 
 if(milesRan != 0 && timeRan == ""){
- timeRan = prompt("Please enter a larger numerical value for how long it took you to run, unless you are Usain Bolt!!",20);
+ timeRan = prompt("Please enter a larger numerical value for how long it took you to run, unless you are Usain Bolt!!",20); //used for input validation
  milesRan = prompt("Please enter the miles you ran.",2);
 }
 if(milesRan == 0 || milesRan == null){
- milesRan = prompt("Please enter a valid number greater then 0",1);
+ milesRan = prompt("Please enter a valid number greater then 0",1); //used for input validation to ensure there is no other values then numerical
 }else if(milesRan >= 1 && timeRan >= 5) {
- console.log("You burned " + totalCalorieburn + " calories on this run");
- console.log("You ran an average of " + avgminperMile + " minutes per mile!");
+ console.log("You burned " + totalCalorieburn + " calories on this run"); //displays the total calories burnt
+ console.log("You ran an average of " + avgminperMile + " minutes per mile!"); // displays the average minutes per mile ran
 
 }
 
