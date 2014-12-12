450486b48e2e5311ebc67390a95bcd93b2990aed
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-12 01:35:23 -0500

    Conditionals_Personal comments written
    Comments added to help clarify the lines and what they do.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -6,11 +6,11 @@ Conditionals Personal
 
 // Calculates your average mile per hour on your drive to work!
 
-var milesWork = prompt("How many miles do you drive to work?",20);
-var timetoDrive = prompt("How long does it take to drive to work in minutes?",30);
+var milesWork = prompt("How many miles do you drive to work?",20); //prompts the user for their travel distance in miles to work
+var timetoDrive = prompt("How long does it take to drive to work in minutes?",30); // prompts the user for the time it takes in minutes to drive that distance
 
 
-if(milesWork == "" || milesWork == null){
+if(milesWork == "" || milesWork == null){ // if mileswork variable is equal to blank or null values it prompts the user to re enter the value
  milesWork = prompt("Please enter a number value for miles you drove to work!");
 }else if(timetoDrive == "" || timetoDrive == null){
  timetoDrive = prompt("Please enter a numerical value for the time it took you to drive to work in minutes!");
