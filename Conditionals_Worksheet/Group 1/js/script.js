d73a25b21270e2a9aae8367f4b11881759189a01
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-11 23:13:53 -0500

    Conditionals_Worksheet Group 1 comments
    Conditionals worksheet updated to include a working formula and
    comments.

--- Conditionals_Worksheet/Group 1/js/script.js
+++ Conditionals_Worksheet/Group 1/js/script.js
@@ -5,11 +5,13 @@ Last Chance for Gas
 
  //This determines if you can make it across the desert with your current fuel
  var gasEfficiency = 20; //This is your miles per gallon your car gets
- var gaugeReading = 75; //this is your gas gauge reading
- var gastankSize = 14; //this is your gas tank size in gallons
+ var gaugeReading = 95;//this is your gas gauge reading percentage
+ var gastankSize = 15; //this is your gas tank size in gallons
 
-if(gastankSize * .75 * 20 >= 200){
+
+if(gastankSize * .95 * gasEfficiency >= 200){ // If your gastank size muliplied by your mpg is greater than or equal to 200 you can make it!
  console.log("Yes you can make it without stopping for gas!");
 }else{
- console.log("No you only have " + gastankSize *.75 + " gallons of gas in your tank, you better get gas now!");
+ console.log("No you only have " + parseInt(gastankSize *.95) + " gallons of gas in your tank, you better get gas now!");
+ // multiplies your gauge reading percentage by the gas tank size to display how many gallons you have.
 }
