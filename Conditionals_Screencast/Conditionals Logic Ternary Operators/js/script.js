b3d856f6573ebf7b9d5c8f5733774afaec7f2f65
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-12 01:00:08 -0500

    Conditionals Screencast Ternary Operations
    Activity Complete

---
+++ Conditionals_Screencast/Conditionals Logic Ternary Operators/js/script.js
@@ -0,0 +1,16 @@
+/*
+Terry Dietrick 11 Dec 2014
+Conditionals Screencasts
+Conditional Logic - Ternary Operations
+
+ */
+
+var age = 11;
+var book;
+
+//if the child is under 10 they get green eggs and ham. Otherwise, they get the time machine
+
+
+book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log(book);
+
