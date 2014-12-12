f13f575de6354292cd0e684999699c8ef8544af0
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-12 00:13:33 -0500

    Conditionals_Worksheet Group 3 comments
    Completed the comments for the group 3 assignment to reflect correctly
    what each line does.

--- Conditionals_Worksheet/Group 3/js/script.js
+++ Conditionals_Worksheet/Group 3/js/script.js
@@ -3,20 +3,21 @@
 Movie Ticket Price
  */
 
+//This calculates the movie ticket price based on age and movie time
 
 
-
- var ageAttendee = prompt("How old are you?",25);
- var movieTime = prompt("What time is your movie?",3);
- var movieTicket = 12;
+ var ageAttendee = prompt("How old are you?",25); //prompts the user for their age
+ var movieTime = prompt("What time is your movie?",3); //prompts the user for their desired movie time
+ var movieTicket = 12; // assigns the defaul ticket price as $12.00
 
 
 if(ageAttendee >=55 || ageAttendee <= 10) {
  console.log("The ticket price is $7.00");
-
+// determines if the moviegoer falls in the discounted ticket price age group
 }else if(movieTime >= 3 && movieTime <= 5){
  console.log("The ticket price is $7.00");
-
+//determines if the moviegoer is buying a ticket for a discounted time line
 }else{
  console.log("The ticket price is $" + movieTicket);
+ //prints out the default ticket price if the moviegoer doesn't fall in any of the previous categories
 }
