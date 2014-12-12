efd26b49167b608c419a74c5dd2723c7deb208be
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-12 00:09:55 -0500

    Conditionals_Worksheet group 3 updated
    Conditionals worksheet group 3 is updated with the coding

--- Conditionals_Worksheet/Group 3/js/script.js
+++ Conditionals_Worksheet/Group 3/js/script.js
@@ -1,19 +1,22 @@
-/*Terry Dietrick 07 Dec 2014 Average Shopping Bill
-
+/*Terry Dietrick 11 Dec 2014
+ SDI Section 04WDD
+Movie Ticket Price
  */
 
- var shoppingArray = new Array(); // establishes the array
- shoppingArray[0] = 127; // week 1 total spent on groceries
- shoppingArray[1] = 85; // week 2 total spent on groceries
- shoppingArray[2] = 178; // week 3 total spent on groceries
- shoppingArray[3] = 114; // week 4 total spent on groceries
- shoppingArray[4] = 156; // week 4 total spent on groceries
-
- var totalSpent = (shoppingArray[0] + shoppingArray[1] + shoppingArray[2] + shoppingArray[3] + shoppingArray[4]);
- // var total spent is equal to each element in the array addded up
- var weeklyAverage = (totalSpent / 5); // divides the total by 5, or the number of weeks to get the average
-
- console.log("You have spent a total of $" + totalSpent + " on groceries over 5 weeks. That is an average of $" + weeklyAverage + " per week.");
- // prints the total spent over 5 weeks and the average spent each week.
 
 
+
+ var ageAttendee = prompt("How old are you?",25);
+ var movieTime = prompt("What time is your movie?",3);
+ var movieTicket = 12;
+
+
+if(ageAttendee >=55 || ageAttendee <= 10) {
+ console.log("The ticket price is $7.00");
+
+}else if(movieTime >= 3 && movieTime <= 5){
+ console.log("The ticket price is $7.00");
+
+}else{
+ console.log("The ticket price is $" + movieTicket);
+}
