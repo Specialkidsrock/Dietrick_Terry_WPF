8017f28f4b37b5c8f009a9db7d56249abcab23fb
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-11 19:45:20 -0500

    Expressions_Worksheet corrected
    All activities corrected to show work is done in a js file instead of
    directly in the html file.

--- Expression_Worksheet/Discounts/js/script.js
+++ Expression_Worksheet/Discounts/js/script.js
@@ -1,2 +1,14 @@
-//
+/*Terry Dietrick 07 Dec 2014 Discounts
+
+ */
+
+
+var itemPrice = 100.24; // sample item price
+var itemDescription = "Awesome Coolguy ninja Statue"; // item description
+var taxSales = 1.07; //sales tax
+var discountPercent = 10; //item discount percentage
+var itemPricewithtax = (itemPrice * taxSales * .9); //item multiplied by sales tax and .9 representing 10% discount
+var itemPricenotax = (itemPrice * .9); // item multiplied without tax and .9 representing 10% discount
+
+console.log("Your " + itemDescription + " was originally $" + itemPrice + ", but after a " + discountPercent + "% discount, it is now $" + itemPricenotax + " without tax, and $" + itemPricewithtax + " with tax.");
 
--- Expression_Worksheet/Dog Years/js/script.js
+++ Expression_Worksheet/Dog Years/js/script.js
@@ -1,2 +1,14 @@
-//
+/*Terry Dietrick 07 Dec 2014 Dog Years
 
+ */
+
+var humanYears = prompt("Enter Sparky's age in human years.",0);
+//Prompts the user for Sparky's age in human years
+
+var dogYears; // Variable for storing the dog years
+
+dogYears = humanYears * 7; // Calculates the dogYears by multiplying humanYears var by 7
+
+
+console.log("Sparky is " + humanYears + " in human years, which is " + dogYears + " in dog years.");
+// Prints result in the console
