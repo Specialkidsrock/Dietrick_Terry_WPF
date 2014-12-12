612b94cf3be5453f50ad02a1c42e46eb75e70f84
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-11 21:31:21 -0500

    Conditionals_Relationals Expressions exercise
    Completed this exercise

--- Conditionals_Screencast/js/script.js
+++ Conditionals_Screencast/js/script.js
@@ -1,15 +1,18 @@
 /*
 Terry Dietrick 11 Dec 2014
 Conditionals Screencasts
+Conditional Logic - Relational Expressions
 
  */
 
 
-var oldEnough = false;
+var kidHeight = 50;
+var minHeight = 48;
+
 
 //if the child is old enough, print to the console,"You can ride!"
-
-if(oldEnough){
+// if the kid is over 48 inches in height
+if(kidHeight > minHeight){
     // code performed if condition is true
     console.log("You can ride the coaster!");
 }
