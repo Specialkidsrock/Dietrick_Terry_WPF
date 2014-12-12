fa4d6f76d7edafd906758ef45bf0a4d72bf70506
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-11 21:38:13 -0500

    Conditionals_Screecast if and else expression
    Screencast practice complete

--- Conditionals_Screencast/js/script.js
+++ Conditionals_Screencast/js/script.js
@@ -6,14 +6,16 @@ Conditional Logic - With an Expression
  */
 
 
-var kidHeight = 47;
+var kidHeight = 52;
 var minHeight = 48;
-var sneakerLift = 2;
+
 
 //if the child is old enough, print to the console,"You can ride!"
 // if the kid is over 48 inches in height
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight > minHeight){
     // code performed if condition is true
     console.log("You can ride the coaster!");
+}else{
+    //code performed if condition is false
+    console.log("Sorry kid, you've got some growing to do!")
 }
-
