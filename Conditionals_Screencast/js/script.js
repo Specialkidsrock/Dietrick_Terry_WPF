d135e33689c1b61b0808b1d864836b6f2c48bb12
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-11 21:33:57 -0500

    Conditionals_Logic with an expressions screencast
    Completed this screencast

--- Conditionals_Screencast/js/script.js
+++ Conditionals_Screencast/js/script.js
@@ -1,19 +1,19 @@
 /*
 Terry Dietrick 11 Dec 2014
 Conditionals Screencasts
-Conditional Logic - Relational Expressions
+Conditional Logic - With an Expression
 
  */
 
 
-var kidHeight = 50;
+var kidHeight = 47;
 var minHeight = 48;
-
+var sneakerLift = 2;
 
 //if the child is old enough, print to the console,"You can ride!"
 // if the kid is over 48 inches in height
-if(kidHeight > minHeight){
+if(kidHeight + sneakerLift > minHeight){
     // code performed if condition is true
     console.log("You can ride the coaster!");
 }
-console.log("What comes after?");
+
