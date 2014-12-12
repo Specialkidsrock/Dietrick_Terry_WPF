747e2623397ca51358abee4724394b9e875bfdf8
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-11 20:17:46 -0500

    Expressions_Wacky created
    Created the expressions wacky assignment, and assigned the variables
    with the greeting strings.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -5,4 +5,20 @@ SDI Section 04WDD
 Expressions Wacky
  */
 
-alert("Testing")
+
+    var greetingText = "Welcome to the Fat Cat"; // first part of the greeting message
+    var greetingFood = " food cost per year calculator"; // second part of the greeting message
+    var concatStrings = greetingText + "" + greetingFood; //concatenates the greeting strings together
+
+    alert(concatStrings); // pop up message to greet the user
+
+
+
+    var howManycats = prompt("How many cats do you own?");
+    var howmuchFood = prompt("How many times a day do you feed them?");
+    var howoldCats = prompt("How old are your cat(s)?");
+
+    var servingSize = 75; // 75 grams is the average feeding size for a regular cat daily
+    var bagSize = 16; // The average cat food bag found online was 16 lbs.
+
+
