912b1654bfa22064947fac691342957f62b59537
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-11 21:43:43 -0500

    Conditionals_Screencast elseif exercise
    Completed this exercise

--- Conditionals_Screencast/js/script.js
+++ Conditionals_Screencast/js/script.js
@@ -1,21 +1,25 @@
 /*
 Terry Dietrick 11 Dec 2014
 Conditionals Screencasts
-Conditional Logic - With an Expression
+Conditional Logic - else if
 
  */
 
 
-var kidHeight = 52;
+var kidHeight = 46;
 var minHeight = 48;
-
+var wParentHeight = 45; //the height of the kid with the parent
 
 //if the child is old enough, print to the console,"You can ride!"
 // if the kid is over 48 inches in height
 if(kidHeight > minHeight){
-    // code performed if condition is true
+    // you can ride!
     console.log("You can ride the coaster!");
+}else if(kidHeight > wParentHeight){
+    //you can ride with a parent present!
+    console.log("You can ride, but with a parent present!");
 }else{
-    //code performed if condition is false
-    console.log("Sorry kid, you've got some growing to do!")
+   //sorry you have growing to do
+    console.log("Sorry kid, you've got some growing to do!");
 }
+
