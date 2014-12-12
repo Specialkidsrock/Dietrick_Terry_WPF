a00aebc3ac617c0fea58f800ef2130f02576cd11
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-11 23:45:15 -0500

    Conditional_Worksheet Group 2
    Comments explaining what each line does has been updated.

--- Conditionals_Worksheet/Group 2/js/script.js
+++ Conditionals_Worksheet/Group 2/js/script.js
@@ -5,7 +5,7 @@
 
 //This calculates your letter grade by your inputted percentage!
 
-var percentGrade = prompt("Please enter your grade percentage",85);
+var percentGrade = prompt("Please enter your grade percentage",85); // Asks the user for their grade percentage
 
 
 if(percentGrade >=95){
@@ -14,22 +14,22 @@ if(percentGrade >=95){
 
 }else if(percentGrade >= 90 && percentGrade <= 94 ){
  console.log("You have a " + percentGrade + "% which means you have earned A in the class!");
-
+//this is an A grade
 }else if(percentGrade >= 85 && percentGrade <= 89 ){
  console.log("You have a " + percentGrade + "% which means you have earned B+ in the class!");
-
+//this is an B + grade
 }else if(percentGrade >= 80 && percentGrade <=84){
  console.log("You have a " + percentGrade + "% which means you have earned B in the class!");
-
+//this is an B grade
 }else if(percentGrade >= 76 && percentGrade <= 79){
  console.log("You have a " + percentGrade + "% which means you have earned C+ in the class!");
-
+//this is an C + grade
 }else if(percentGrade >= 73 && percentGrade <= 75){
  console.log("You have a " + percentGrade + "% which means you have earned C in the class!");
-
+//this is an C grade
 }else if(percentGrade >= 70 && percentGrade <= 72){
  console.log("You have a " + percentGrade + "% which means you have earned D in the class!");
-
+//this is an D grade
 }else if(percentGrade <= 69){
  console.log("You have a " + percentGrade + "% which means you have earned a failing grade in the class!");
 }
