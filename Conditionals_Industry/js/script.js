a69a5b7d83b7f203871ccaaad970a8bec3f2ab02
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-12 02:15:32 -0500

    Conditionals_Industry added ternary
    Added a ternary and input validation if/else if lines of code

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -6,7 +6,17 @@ Conditionals Industry
 alert("Is your monitor 4K resolution? Enter the values ");
 var horizontalPixelsvalue = prompt("Please enter your monitors vertical pixel value", 800);
 var verticalPixelvalue = prompt("Please enter your monitors horizontal pixel value",1200);
-
+var totalResolution = (horizontalPixelsvalue * verticalPixelvalue):
 var uhdResolution = 8294400;
+var yourDisplay;
 
-If
+
+if(horizontalPixelsvalue == "" || horizontalPixelsvalue == null){
+ horizontalPixelsvalue = prompt("Please enter a numerical value for your horizontal pixels!!");
+
+}else if(verticalPixelvalue == "" || verticalPixelvalue = null){
+ verticalPixelvalue = prompt("Please enter a numerical value for your vertical pixel amount!");
+}
+
+yourDisplay= (totalResolution < uhdResolution) ? "Your monitor is not a 4K resolution monitor." : "Your monitor is a 4k resolution monitor!";
+console.log(yourDisplay);
