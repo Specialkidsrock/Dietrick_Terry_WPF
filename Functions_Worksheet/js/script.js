08729ae9c4a9078fd2b2ce63603878feba7ef2ee
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-18 21:50:58 -0500

    Function - Worksheet
    Changed the variable name inside the function to better represent what
    is being calculated

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -12,7 +12,7 @@ Functions - Worksheet
 var totalCirc = calcCircumference(3); //Establishes the result variable
 
 
-function calcCircumference(radius){ // function used to solve the equation
+function calcCircumference(radius){ // Function used to solve the equation
         var circumference = radius * Math.PI * 2; //Math.PI used to represent PI instead of typing the #
 
         return circumference; // Spits the result out to the result variable
@@ -20,16 +20,16 @@ function calcCircumference(radius){ // function used to solve the equation
 
 
 console.log(" The circumference of the circle is " + totalCirc + ".");
-//prints the circumference to the console using the result variable
+//Prints the circumference to the console using the result variable
 
 
 // Calculates how many bee stings are needed to kill an animal in a function
 
 var totalStings = stingCalc(100); // Establishes the result variable
 
-function stingCalc(weight){ // creates the function inputting the argument into the parameter of the function
-        var stingerKiller = 8.666666667 * (weight); // calculates how many stings per pound are needed to kill an animal
-        return stingerKiller; // Spits the result to the result variable declared outside the function
+function stingCalc(weight){ // Creates the function inputting the argument into the parameter of the function
+        var stingsToKill = 8.666666667 * (weight); // Calculates how many stings per pound are needed to kill an animal
+        return stingsToKill; // Spits the result to the result variable declared outside the function
 }
 
 console.log("It takes " + parseInt(totalStings) + " bee stings to kill this animal.");
