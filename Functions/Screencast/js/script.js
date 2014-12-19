91af3e2582a45271b54349b06e0827af758aad7c
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-18 20:09:23 -0500

    Functions - Returning Values
    This screencast exercise demonstrated how to return values and how to
    print the returned values out to the variables they are stored into.

--- Functions/Screencast/js/script.js
+++ Functions/Screencast/js/script.js
@@ -1,17 +1,16 @@
 /*Terry Dietrick
 18 Dec 2014
 SDI Section 04WDD
-Functions - Parameters and Arguments
+Functions - Returning Values
  */
 
+var total = calcArea(30, 20);
 
+function calcArea(w, h){
+    var area = w * h;
+    console.log(area);
+    return area; //function spitting the information out
 
-function dogYears(age = 4){ //parameters
-    var age = 4;
-    var dogYears = age * 7;
-    console.log("Sparky is " + dogYears + " years old.");
 }
 
-var age1 = 4;
-dogYears(age1); //arguments
-dogYears(6);
+console.log(total);
