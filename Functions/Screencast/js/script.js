3536cd39b8e4250cd9263bfa98d1355e82445ff9
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-18 19:41:10 -0500

    Functions Basic Structure
    JS File created showing the basic structure of a function

--- Functions/Screencast/js/script.js
+++ Functions/Screencast/js/script.js
@@ -1,7 +1,18 @@
 /*Terry Dietrick
 18 Dec 2014
 SDI Section 04WDD
-Functions
+Functions - Basic Structure
  */
 
 //This
+
+function outptMsg(){
+    console.log("Hello World");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
