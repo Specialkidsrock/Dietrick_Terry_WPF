618b188716ae7dda2164c3958faff0fa924e5774
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-19 08:27:39 -0500

    Assignment - Functions
    Comments added to every line.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -16,24 +16,24 @@ gbtoMB = sdCardSize * 1000; // Converts the SD Card size from MB to GB
 totalPictures = gbtoMB / pictureSize - pictures; // Calculates how many more pictures total the user could fit on their SD Card
 spaceUsed = totalPictures / gbtoMB * 100; // This calculates the percentage of used space on the SD Card currently
 
-console.log(" You could fit approximately " + parseInt(totalPictures) + " more pictures on this SD Card.");
-console.log("Your current pictures use about " + parseInt(spaceUsed) + "% of the space available on this SD Card.");
+console.log(" You could fit approximately " + parseInt(totalPictures) + " more pictures on this SD Card."); // Displays to the user how many more pictures they can fit on their SD Car
+console.log("Your current pictures use about " + parseInt(spaceUsed) + "% of the space available on this SD Card."); //Displays the current percentage of spaced used on the SD Card.
 
 var percentSpace = convertToFraction(.2); //invoke for the function convertToFraction
 
 
 function convertToFraction(fraction) {//anonymous function to convert a decimal to a fraction
-    return (fraction * 100) + "%"; // 
+    return (fraction * 100) + "%"; // Multiplies the decimal by 100 and adds the % symbol, then spits this out to the invoke call.
 }
 
-console.log(percentSpace);
+console.log(percentSpace); // Prints to the console the result of the anonymous function
 
 
-var calcSpace = function(a, b, c) {
-    var gbToMB = a * 1000;
-    var totalspaceRequired = ((gbToMB / b) - c);
-    return (totalspaceRequired);
+var calcSpace = function(a, b, c) { //Defined a 3 parameter function to solve for the same problem as above
+    var gbToMB = a * 1000; //Converts GB to MB
+    var totalspaceRequired = ((gbToMB / b) - c); //Calculates how many more pictures total the user could fit on their SD Card
+    return (totalspaceRequired); //Spits the value of totalSpaceRequired variable out to an invoke call
 }
 
-console.log(parseInt(calcSpace(2,12,120)));
+console.log(parseInt(calcSpace(2,12,120))); // Invokes the function for what value was calculated and parses the number for a whole integer.
 
