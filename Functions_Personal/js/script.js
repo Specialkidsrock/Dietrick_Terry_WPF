ffc4ec6aee9ee5dfe2b1caed6dd631a49982990b
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-19 00:00:38 -0500

    Assignment - Functions
    if and else if updated, still trying to figure out how to work in a
    function

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -7,21 +7,24 @@ Functions - Personal
 // Calculates the cost of gas and miles per tank of gas based off your miles per gallon
 
 
-var cityHwy = prompt("Do you drive mostly in the City or on the Hwy?" + <Br>  + "Enter 1 for city, or 2 for hwy");
+var cityHwy = prompt("Do you drive mostly in the City or on the Hwy?" + "<br>" + "Enter 1 for city, or 2 for hwy");
 var gasPrice = 2.477; //nationwide average cost as reported by AAA
 
+
+
+
 if(cityHwy = 1 && cityHwy != 2){
+
     var mpgCity = prompt("What is your estimated MPG");
     var tanksizeCity = prompt("What is your gas tank size");
     var totalCostCity = gasPrice * tanksizeCity;
-    var totalGascity = tanksizeCity * mpgCity;
-
+    var totalGasCity = tanksizeCity * mpgCity;
+    console.log(totalCostCity)
 
 }else if(cityHwy = 2 || cityHwy != 1){
     var mpgHwy = prompt("What is your estimated MPG");
     var tanksizeHwy = prompt("What is your gas tank size");
-    var totalCostHwy = gasPrice * tanksizeHwy
+    var totalCostHwy = gasPrice * tanksizeHwy;
     var totalGasHWy = tanksizeHwy * mpgHwy;
     console.log(totalCostHwy)
 }
-console.log
