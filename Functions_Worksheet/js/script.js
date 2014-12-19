f3bf2b8364b641a2cf1598458e9798727fb0cbee
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-18 20:41:16 -0500

    Functions - Worksheet
    Comments added for the first equation, explains the calculations
    required

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -1,6 +1,10 @@
 /*Terry Dietrick
-Dec 2014
+18 Dec 2014
 SDI Section 04WDD
 Functions - Worksheet
  */
+
+//Calculate the circumference of a circle
+
+// The circumference of a circle = 2 * (pie) * radius
 
