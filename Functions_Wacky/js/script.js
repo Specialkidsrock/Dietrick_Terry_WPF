6b11da6de03a15822cbf1752ec7e7ceceb8b903d
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-19 09:34:45 -0500

    Assignment - Functions
    If and Else If statements created, variables assigned correct values

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -10,15 +10,24 @@ Functions - Wacky
 var yearsIn = prompt("How many years have you been in the Army?", 10);
 var yearsLeft = prompt("How many years do you think you have left before you decide to get out?", 2);
 var dayOfYear = prompt("What day of the year is it? (1-365)",353);
+
+
+if(yearsIn == 0 || yearsIn == ""){
+    yearsIn = prompt("How many years have you been in the Army?", 10);
+
+}else if(yearsLeft == 0 || yearsLeft == ""){
+    yearsLeft = prompt("How many years do you think you have left before you decide to get out?", 2);
+
+}else if(dayOfYear == 0 || dayOfYear == ""){
+    dayOfYear = prompt("What day of the year is it? (1-365)",353);
+
+}
+
 var daysInaYear = 365;
 var hoursInaDay = 24;
 var daysLeft = yearsLeft * daysInaYear - dayOfYear;
 var daysIn = daysInaYear * yearsIn;
 var hoursSpentIn = hoursInaDay * daysIn;
-
-
-
-
 
 console.log("You have spent " + hoursSpentIn + " hours of your life in the Army!");
 console.log("You have " + daysLeft + " days left in the Army, or " + (daysLeft * hoursInaDay) + " hours left in the Army.");
