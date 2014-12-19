220cb9e91208be5fa4e8e19e9b929161659233cf
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-18 22:52:52 -0500

    Assignment - Functions
    All folders and files have been correctly named and formatted

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,31 +1,5 @@
 /*Terry Dietrick
 18 Dec 2014
 SDI Section 04WDD
-Loops - While
+Functions - Personal
  */
-
-var b = 10; // sets up the index
-
-while (b > 0){ // checks the condition
-    console.log(b + " kegs on the wall");
-    b--; //decrements or increments the index
-}
-
-// Do While loop
-
-var c = 10;
-
-do{
-    console.log(c + "kegs on the wall");
-    c--;
-
-}
-
-while(c > 0);
-
-// For Loop
-
-for (var i = 10; i > 0; i--){
-        console.log(i + " kegs on the waller");
-
-}
