bb3480585f4eedb9177258632be2635a683f0e54
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-19 01:47:23 -0500

    Assignment - Functions
    Industry initial lines of code input

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -3,3 +3,19 @@
 SDI Section 04WDD
 Functions - Industry
  */
+
+// calculates the amount of pictures you can store on an SD card
+
+
+var sdCardSize = prompt("Enter your SD Card size in GB",2);
+var pictureSize = prompt("How large is your average digital picture?");
+var picture = prompt("How man pictures have you taken?",34);
+
+var sdCard = function(pictures,pictureSize, mbtoGB) {
+    mbtoGB = 1000;
+    var totalspaceRequired = (pictures * pictureSize * mbtoGB);
+    return totalspaceRequired;
+}
+
+var calcSpace = ;
+console.log(calcSpace);
