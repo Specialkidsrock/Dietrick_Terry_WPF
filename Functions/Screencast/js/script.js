9ea02a34b55f5cb9ae1402d91c10f8f51ceb7090
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-18 19:49:55 -0500

    Function Variable Scope
    Depicts how variables act inside and outside of functions

--- Functions/Screencast/js/script.js
+++ Functions/Screencast/js/script.js
@@ -1,22 +1,22 @@
 /*Terry Dietrick
 18 Dec 2014
 SDI Section 04WDD
-Functions - Function Execution
+Functions - Variable Scope
  */
 
 
 
-function outptMsg(){
-    console.log("Hello World");
-}
+var width = 5;
+
 
 function calcArea(){
     var width = 20;
     var height = 30;
     var area = width * height;
-    console.log(area);
+    //console.log(area);
 }
 
 calcArea();
-calcArea();
-calcArea();
+console.log(width);
+
+
