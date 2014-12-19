0cfcde3a7f33a0d5a6a515053bb92fc9d35f9de4
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-18 22:00:51 -0500

    Screen Cast - Loops
    While loop demonstrated

--- Loops/Screencast/js/script.js
+++ Loops/Screencast/js/script.js
@@ -1,7 +1,12 @@
 /*Terry Dietrick
 18 Dec 2014
 SDI Section 04WDD
-Loops -
+Loops - While
  */
 
+var b = 50; // sets up the index
 
+while (b > 0){ // checks the condition
+    console.log(b + " kegs on the wall");
+    b--; //decrements or increments the index
+}
