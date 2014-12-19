6b2b45b4e85233f4d3cc2e1c1a79fa0914e02d79
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-19 09:14:05 -0500

    Assignment - Functions
    Wacky calculator initial variables and values inputted.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -3,3 +3,24 @@
 SDI Section 04WDD
 Functions - Wacky
  */
+
+// calculates how many days left in the army, and how many days you have been in the army
+
+
+var yearsIn = prompt("How many years have you been in the Army?", 10);
+var yearsLeft = prompt("How many years do you think you have left before you decide to get out?", 2);
+var dayOfYear = prompt("What day of the year is it? (1-365)",353);
+var daysInaYear = 365;
+var minutesInaDay = 3600;
+var daysLeft;
+var daysIn;
+var hoursSpentIn;
+
+daysIn = daysInaYear * yearsIn;
+daysLeft = yearsLeft * daysInaYear - dayOfYear;
+
+console.log
+
+function depressing(longHours,yearsToGo){
+
+}
