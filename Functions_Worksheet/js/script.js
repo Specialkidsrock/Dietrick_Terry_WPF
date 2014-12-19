6fd2d1ae0a18e7aba179b3ac2299af61d0804f10
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-18 20:58:51 -0500

    Functions - Worksheet
    Input the function with no comments to establish the work flow

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -10,3 +10,10 @@ Functions - Worksheet
 
 var radius = prompt("Please enter the radius"); //Prompts the user for the radius
 
+function calcCircumference(r){
+        var circumference = r * 3.1419 * 2;
+        return circumference;
+        console.log("The circumference of the circle is " + circumference + ".");
+
+}
+
