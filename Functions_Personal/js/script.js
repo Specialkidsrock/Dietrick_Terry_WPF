6d3b1a9ca4c70ae8cac1d85c7e28e7aaffba3dd8
Specialkidsrock <tldietrick@fullsail.edu>
2014-12-19 00:24:17 -0500

    Assignment - Functions
    Personal calculation function completed, I am not happy and will
    probably change it up, but I can’t think of anything creative.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -7,24 +7,27 @@ Functions - Personal
 // Calculates the cost of gas and miles per tank of gas based off your miles per gallon
 
 
-var cityHwy = prompt("Do you drive mostly in the City or on the Hwy?" + "<br>" + "Enter 1 for city, or 2 for hwy");
+var cityVsHwy = prompt("Do you drive mostly in the City or on the Hwy?" + "<br>" + "Enter 1 for city, or 2 for hwy");
 var gasPrice = 2.477; //nationwide average cost as reported by AAA
 
 
 
 
-if(cityHwy = 1 && cityHwy != 2){
+
+if(cityVsHwy = 1 && cityVsHwy != 2){
 
     var mpgCity = prompt("What is your estimated MPG");
     var tanksizeCity = prompt("What is your gas tank size");
     var totalCostCity = gasPrice * tanksizeCity;
     var totalGasCity = tanksizeCity * mpgCity;
-    console.log(totalCostCity)
-
-}else if(cityHwy = 2 || cityHwy != 1){
+    console.log("Your total cost to fill up your gas tank would be $" + totalCostCity + ".");
+    console.log("You could travel approximately " + totalGasCity +" miles before you would run out of gas");
+}else if(cityVsHwy = 2 || cityVsHwy != 1){
     var mpgHwy = prompt("What is your estimated MPG");
     var tanksizeHwy = prompt("What is your gas tank size");
     var totalCostHwy = gasPrice * tanksizeHwy;
     var totalGasHWy = tanksizeHwy * mpgHwy;
-    console.log(totalCostHwy)
+    console.log("Your total cost to fill up your gas tank would be $" + totalCostHwy + ".");
+    console.log("You could travel approximately " + totalGasHWy + " miles before you ran out of gas.");
 }
+
